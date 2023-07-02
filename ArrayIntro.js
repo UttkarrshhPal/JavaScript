782e727d47a5461bf31a587e5ee169b65b2605dc
Utkarsh_Pal <110682756+UttkarrshhPal@users.noreply.github.com>
2023-07-02 22:48:42 +0530

    Create ArrayIntro.js

---
+++ ArrayIntro.js
@@ -0,0 +1,26 @@
+
+// Arrays
+let arr1 = [1,2,3,'name']
+let arr2 = [3,4,5]
+//Access
+let  n= arr1[3];
+console.log(n);
+// Modify the Array
+arr1[3] = 'age';
+console.log(arr1);
+//Array Method
+
+// Join Method
+console.log(arr1.join(''))
+console.log(arr1)
+// IndexOf
+console.log(arr1.indexOf('age'))
+//concat method
+console.log(arr1.concat(arr2))
+// push method
+console.log(arr1.push(5))
+console.log(arr1)
+// pop method
+console.log(arr1.pop())
+// lenght method
+console.log(arr1.length)
