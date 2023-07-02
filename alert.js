6e1870ad15c3facbb85123e9cc3c7708755b88c5
Utkarsh_Pal <110682756+UttkarrshhPal@users.noreply.github.com>
2023-07-02 22:50:59 +0530

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,5 @@
+alert('dankrishu');
+let number = prompt('Enter number', 'number goes here')
+console.log(number)
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree) // result will be true or false based on what you click on the dialog box
